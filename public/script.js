3068472aa7d22504e5fda85d408c2280d0270fe9
Azfazaki Hakimi <azfazakihakimi1@gmail.com>
2026-02-05 11:16:25 +0800

    save

--- public/script.js
+++ public/script.js
@@ -1,5 +1,29 @@
-firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+    apiKey: "AIzaSyC2DIO1IrQwn_vNadYlwnI5oj4oe5W0IxM",
+    authDomain: "aspedo-7753e.firebaseapp.com",
+    databaseURL: "https://aspedo-7753e-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    projectId: "aspedo-7753e",
+    storageBucket: "aspedo-7753e.firebasestorage.app",
+    messagingSenderId: "246861248788",
+    appId: "1:246861248788:web:fa14df35f9e9e57ca3ea01"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 const db = firebase.database();
+
+const PASSWORDS = {
+    'MC': 'mc1',
+    'Konsum': 'konsum1',
+    'Perkap': 'perkap1',
+    'Band': 'band1',
+    'PDD': 'pdd1',
+    'ADMIN': 'admin1'
+};
+
+let currentTarget = null;
+let currentDivision = null;
 
 const views = {
     landing: document.getElementById('landing-view'),
@@ -19,47 +43,56 @@ const ui = {
     targetBtns: document.querySelectorAll('.target-btn')
 };
 
-let currentTarget = null;
-let currentDivision = null;
-
 function switchView(viewName) {
-    Object.values(views).forEach(el => el.classList.remove('active'));
-    views[viewName].classList.add('active');
+    Object.values(views).forEach(el => {
+        if(el) el.classList.remove('active');
+    });
+
+    if(views[viewName]) views[viewName].classList.add('active');
 }
 
-function openLogin(target) {
+window.openLogin = function(target) {
     currentTarget = target;
-    ui.loginTitle.innerText = target === 'ADMIN' ? 'Login Admin' : `Login ${target}`;
-    ui.passInput.value = '';
+    if(ui.loginTitle) ui.loginTitle.innerText = target === 'ADMIN' ? 'Login Admin' : `Login ${target}`;
+    if(ui.passInput) ui.passInput.value = '';
+    
     views.login.classList.add('active');
-    ui.passInput.focus();
+    setTimeout(() => {
+        if(ui.passInput) ui.passInput.focus();
+    }, 100);
 }
 
-function closeLogin() {
+window.closeLogin = function() {
     views.login.classList.remove('active');
 }
 
-ui.loginBtn.addEventListener('click', () => {
-    const input = ui.passInput.value;
-    if (SECRETS.passwords[currentTarget] && input === SECRETS.passwords[currentTarget]) {
-        closeLogin();
-        if (currentTarget === 'ADMIN') {
-            initAdminPanel();
+if(ui.loginBtn) {
+    ui.loginBtn.addEventListener('click', () => {
+        const input = ui.passInput.value;
+        
+        if (PASSWORDS[currentTarget] && input === PASSWORDS[currentTarget]) {
+            closeLogin();
+            if (currentTarget === 'ADMIN') {
+                initAdminPanel();
+            } else {
+                initUserPanel(currentTarget);
+            }
         } else {
-            initUserPanel(currentTarget);
+            alert('Kode akses salah!');
+            ui.passInput.value = '';
         }
-    } else {
-        alert('Kode akses salah!');
-        ui.passInput.value = '';
-    }
-});
+    });
+}
 
 function initAdminPanel() {
     switchView('admin');
     
     ui.targetBtns.forEach(btn => {
-        btn.onclick = () => {
-            const targetDiv = btn.getAttribute('data-target');
+        const newBtn = btn.cloneNode(true);
+        btn.parentNode.replaceChild(newBtn, btn);
+        
+        newBtn.onclick = () => {
+            const targetDiv = newBtn.getAttribute('data-target');
             const message = ui.adminInput.value.trim();
 
             if (!message) {
@@ -72,6 +105,8 @@ function initAdminPanel() {
             }
         };
     });
+    
+    ui.targetBtns = document.querySelectorAll('.target-btn');
 }
 
 function sendMessage(division, msg) {
@@ -79,19 +114,24 @@ function sendMessage(division, msg) {
         message: msg,
         timestamp: Date.now(),
         status: 'active'
+    }).then(() => {
+        alert('Pesan terkirim ke ' + division);
+    }).catch(err => {
+        alert('Error: ' + err.message);
     });
-    alert('Pesan terkirim!');
 }
 
-function clearInput() {
-    ui.adminInput.value = '';
-    ui.adminInput.focus();
+window.clearInput = function() {
+    if(ui.adminInput) {
+        ui.adminInput.value = '';
+        ui.adminInput.focus();
+    }
 }
 
 function initUserPanel(division) {
     currentDivision = division;
     switchView('user');
-    ui.userDivName.innerText = division;
+    if(ui.userDivName) ui.userDivName.innerText = division;
 
     db.ref('divisions/' + division).on('value', (snapshot) => {
         const data = snapshot.val();
@@ -107,8 +147,8 @@ function initUserPanel(division) {
 function setScreenRed(msg) {
     views.user.classList.remove('bg-green');
     views.user.classList.add('bg-red');
-    ui.screenMsg.innerText = msg;
-    ui.confirmBtn.classList.remove('hidden');
+    if(ui.screenMsg) ui.screenMsg.innerText = msg;
+    if(ui.confirmBtn) ui.confirmBtn.classList.remove('hidden');
     
     if (navigator.vibrate) navigator.vibrate([500, 200, 500]);
 }
@@ -116,15 +156,17 @@ function setScreenRed(msg) {
 function setScreenGreen() {
     views.user.classList.remove('bg-red');
     views.user.classList.add('bg-green');
-    ui.screenMsg.innerText = 'STANDBY';
-    ui.confirmBtn.classList.add('hidden');
+    if(ui.screenMsg) ui.screenMsg.innerText = 'STANDBY';
+    if(ui.confirmBtn) ui.confirmBtn.classList.add('hidden');
 }
 
-ui.confirmBtn.addEventListener('click', () => {
-    if (currentDivision) {
-        db.ref('divisions/' + currentDivision).update({
-            status: 'confirmed',
-            message: null 
-        });
-    }
-});
+if(ui.confirmBtn) {
+    ui.confirmBtn.addEventListener('click', () => {
+        if (currentDivision) {
+            db.ref('divisions/' + currentDivision).update({
+                status: 'confirmed',
+                message: null 
+            });
+        }
+    });
+}
