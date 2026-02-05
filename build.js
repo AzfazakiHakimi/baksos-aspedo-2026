4dbe2ce7df292300612e5745a5b58af7ad51f5a4
Azfazaki Hakimi <azfazakihakimi1@gmail.com>
2026-02-05 11:07:25 +0800

    save

--- build.js
+++ build.js
@@ -1,20 +1,23 @@
 const fs = require('fs');
+const path = require('path');
 
-// Kita ambil data dari Environment Variable Vercel
 const firebaseConfig = process.env.FIREBASE_JSON; 
 const passwordsConfig = process.env.PASSWORDS_JSON;
 
 if (!firebaseConfig || !passwordsConfig) {
-    console.error("ERROR: Environment Variables belum disetting di Vercel!");
+    console.error("ERROR: Env Variables belum setting!");
     process.exit(1);
 }
 
-// Kita susun isi file secrets.js
 const content = `const SECRETS = {
     firebase: ${firebaseConfig},
     passwords: ${passwordsConfig}
 };`;
 
-// Tulis file secrets.js
-fs.writeFileSync('secrets.js', content);
-console.log("SUCCESS: secrets.js berhasil dibuat otomatis!");
+if (!fs.existsSync('./public')){
+    fs.mkdirSync('./public');
+}
+
+fs.writeFileSync('./public/secrets.js', content);
+
+console.log("SUCCESS: secrets.js berhasil dibuat di dalam folder public!");
